41cb73ef13690ba16ba5698ba47991c8825a9f1b
Tyler Manning <sylsr9@gmail.com>
2015-10-22 18:53:50 -0600

    Added menu tab

---
+++ GameProject/src/FileTabMenuListener.java
@@ -0,0 +1,18 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+/**
+ * Class that will listen to file tab menu actions.
+ * @author Tyler
+ *
+ */
+public class FileTabMenuListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- GameProject/src/GameLaunch.java
+++ GameProject/src/GameLaunch.java
@@ -1,20 +1,44 @@
 import java.awt.Dimension;
+import java.awt.Menu;
+import java.awt.MenuBar;
 
 import javax.swing.JFrame;
 
-import java.awt.Toolkit;
 
 
+/**
+ * Main class for the top-view shooter game. Created by FIRST Robotics Team 1891.
+ * @author Tyler
+ *
+ */
 public class GameLaunch {
+	/**
+	 * @param args
+	 */
+	@SuppressWarnings("javadoc")
 	public static void main(String[]args){
 		JFrame frame = new JFrame();
 		frame.setPreferredSize(new Dimension(800, 600));
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		launchMenuBar(frame);
 		
 		
 		frame.pack();
 		frame.setVisible(true);
+	}
+	
+	private static void launchMenuBar(JFrame frame){
+		MenuBar frameMenu = new MenuBar();
+		Menu fileTab = new Menu("File");
+		Menu helpTab = new Menu("Help");
+	
+		fileTab.addActionListener(new FileTabMenuListener());
+		
+		
+		frameMenu.add(helpTab);
+		frameMenu.add(fileTab);
+		frame.setMenuBar(frameMenu);
 	}
 
 }
