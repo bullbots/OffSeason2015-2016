d2edc84b932a2ccb117db98a7b96fdfadf6c25f6
Tyler Manning <sylsr9@gmail.com>
2015-10-22 19:00:45 -0600

    Added game launcher

--- GameProject/src/GameLaunch.java
+++ GameProject/src/GameLaunch.java
@@ -1,6 +1,5 @@
 import java.awt.Dimension;
-import java.awt.Menu;
-import java.awt.MenuBar;
+
 
 import javax.swing.JFrame;
 
@@ -21,24 +20,8 @@ public class GameLaunch {
 		frame.setPreferredSize(new Dimension(800, 600));
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		launchMenuBar(frame);
-		
 		
 		frame.pack();
 		frame.setVisible(true);
 	}
-	
-	private static void launchMenuBar(JFrame frame){
-		MenuBar frameMenu = new MenuBar();
-		Menu fileTab = new Menu("File");
-		Menu helpTab = new Menu("Help");
-	
-		fileTab.addActionListener(new FileTabMenuListener());
-		
-		
-		frameMenu.add(helpTab);
-		frameMenu.add(fileTab);
-		frame.setMenuBar(frameMenu);
-	}
-
 }
