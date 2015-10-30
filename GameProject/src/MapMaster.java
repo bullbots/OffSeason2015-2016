f16d0264bca840f3f2ba9ecc30446948277ac308
Tyler Manning <sylsr9@gmail.com>
2015-10-29 18:15:14 -0600

    Added map tester and modifed map code

--- GameProject/src/MapMaster.java
+++ GameProject/src/MapMaster.java
@@ -7,6 +7,8 @@ import java.util.LinkedList;
  *
  */
 public class MapMaster {
+	private static int mapDensity=50;
+	private static boolean[][] occupiedArray = new boolean[800][600];
 	LinkedList<MapBlock> blocks= new LinkedList<MapBlock>();
 	
 	/**
@@ -20,8 +22,32 @@ public class MapMaster {
 	
 	private static void fillList(LinkedList<MapBlock> blocks){
 		blocks.add(new MapBlock(new Point(0,0), 10, 800));
+		fillOccupiedArrayIndex(new Point(0,0), 10, 800);
 		blocks.add(new MapBlock(new Point(0,0), 600, 10));
+		fillOccupiedArrayIndex(new Point(0,0), 600, 10);
 		blocks.add(new MapBlock(new Point(790,0),600, 10 ));
+		fillOccupiedArrayIndex(new Point(790,0),600, 10 );
 		blocks.add(new MapBlock(new Point(0,590),800, 10));
+		fillOccupiedArrayIndex(new Point(0,590),800, 10);
+		for(int i=0; i<mapDensity; i++){
+			
+		}
+	}
+
+	/**
+	 * @return the array with occupied data
+	 */
+	public boolean[][] getOccupiedArray() {
+		return occupiedArray;
+	}
+
+	private static void fillOccupiedArrayIndex(Point start,int numX, int numY){
+		int startX=start.x;
+		int startY=start.y;
+		for(int i=0; i<numX; i++){
+			for(int j=0; j<numY; j++){
+				occupiedArray[i][j] =true;
+			}
+		}
 	}
 }
